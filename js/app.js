4372de8d958bafb96d060f24f75635f3c3456f61
Sam Chung <chungsam@outlook.com>
2016-11-29 15:35:53 -0800

    add game state and drawScoreboard

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
+// TODO:
+// Make game restart if player collides with enemy or reaches coast
+// Add gems and other items to get more points
+// Add start screen
+// Add level end screen
+
+var game = {
+    state: {
+        level: 1,
+        score: 0,
+    }
+}
+
+var drawScoreboard = function(level, score) {
+    ctx.font = "20px Helvetica";
+    ctx.fillText("Level: " + level, 210, 20);
+    ctx.fillText("Score: " + score, 400, 20);
+}
+
+
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,19 +30,17 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 
     this.x = Math.random() * 505
-    this.y =  60 + (82 * Math.round(Math.random() * 2));
-    this.movementSpeed = Math.random() * 2;
+    this.y = 60 + (82 * Math.round(Math.random() * 2));
+    this.movementSpeed = Math.random() * 2 * game.state.level;
 };
 
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
-    var reachRowEnd = false;
 
     if (this.x > 606) {
         this.x = 0;
@@ -28,32 +48,34 @@ Enemy.prototype.update = function(dt) {
         this.x += this.movementSpeed;
     }
 
+    drawScoreboard(game.state.level, game.state.score);
+
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function () {
     this.sprite = 'images/char-boy.png';
-    this.x = (505 / 5) * 2 ; 
+    this.x = (505 / 5) * 2;
     this.y = 400;
 }
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function (dt) {
     this.x * dt;
     this.y * dt;
 }
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.prototype.handleInput = function(inputKey) {
+Player.prototype.handleInput = function (inputKey) {
     var xDistance = 100;
     var yDistance = 82;
 
@@ -76,8 +98,6 @@ Player.prototype.handleInput = function(inputKey) {
     } else {
         console.log("Please use one of the arrow keys");
     }
-
-    console.log(this.x, this.y);
 }
 
 
@@ -86,19 +106,20 @@ Player.prototype.handleInput = function(inputKey) {
 // Place the player object in a variable called player
 var allEnemies = [];
 var player = new Player();
-var loadEnemies = function() {
+var loadEnemies = function () {
     for (i = 0; i < 3; i++) {
         allEnemies.push(new Enemy());
     }
-    
+
 }
 loadEnemies();
 
 
 
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
