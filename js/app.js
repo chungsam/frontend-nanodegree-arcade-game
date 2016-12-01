50d3400fab9377583ced4c0c8cad7ee1fab90780
Sam Chung <chungsam@outlook.com>
2016-12-01 07:36:22 -0800

    add collideWithPlayer

--- js/app.js
+++ js/app.js
@@ -11,13 +11,11 @@ var game = {
     }
 }
 
-var drawScoreboard = function(level, score) {
+var drawScoreboard = function (level, score) {
     ctx.font = "20px Helvetica";
     ctx.fillText("Level: " + level, 210, 20);
     ctx.fillText("Score: " + score, 400, 20);
 }
-
-
 
 
 // Enemies our player must avoid
@@ -49,6 +47,7 @@ Enemy.prototype.update = function (dt) {
     }
 
     drawScoreboard(game.state.level, game.state.score);
+    this.collideWithPlayer();
 
 };
 
@@ -56,6 +55,25 @@ Enemy.prototype.update = function (dt) {
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+/**
+ * Checks whether the Enemy collided with player
+ */
+Enemy.prototype.collideWithPlayer = function () {
+
+    // TODO: adjust sprite width and height
+    var spriteWidth = 100;
+    var spriteHeight = 100;
+    if (player.x >= this.x &&
+        player.x < this.x + spriteWidth &&
+        player.y >= this.y &&
+        player.y < this.y + spriteHeight
+    ) {
+        console.log("COLLIDED!"); // TODO: Remove after testing
+    }
+    // call when player.x is close to enemy.x
+    // put this logic inside of enemy.prototype.update?
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
